425d031dd59aecb2584f1c97efcfa88263ed5cd2
lievincent <vincentxxxlie@yahoo.com>
2019-05-06 09:30:33 +0700

    update by vincent

--- app/src/main/java/com/example/user/myapplication/PopEditLab.java
+++ app/src/main/java/com/example/user/myapplication/PopEditLab.java
@@ -3,21 +3,18 @@ package com.example.user.myapplication;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
 
 /**
  * Created by USER on 15/04/2019.
  */
 
-public class PopLab extends Activity {
+public class PopEditLab extends Activity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
-        setContentView(R.layout.poplab);
+        setContentView(R.layout.popeditlab);
 
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
@@ -25,7 +22,7 @@ public class PopLab extends Activity {
         int width = dm.widthPixels;
         int height = dm.heightPixels;
 
-        getWindow().setLayout((int) (width*.8),(int) (height*.6));
+        getWindow().setLayout((int) (width*.8),(int) (height*.7));
 
     }
 }
--- app/src/main/java/com/example/user/myapplication/TemplateLab.java
+++ app/src/main/java/com/example/user/myapplication/TemplateLab.java
@@ -4,24 +4,54 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 public class TemplateLab extends AppCompatActivity {
 
-    LinearLayout lab1;
+    ImageView editLab1, editLab2, deleteLab1, deleteLab2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_template_lab);
 
-        lab1 = (LinearLayout) findViewById(R.id.lab1);
+        editLab1 = (ImageView) findViewById(R.id.editLab1);
+        editLab2 = (ImageView) findViewById(R.id.editLab2);
+        deleteLab1 = (ImageView) findViewById(R.id.deleteLab1);
+        deleteLab2 = (ImageView) findViewById(R.id.deleteLab2);
 
-        lab1.setOnClickListener(new View.OnClickListener() {
+        editLab1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(TemplateLab.this , PopLab.class));
+                Intent intent = new Intent(TemplateLab.this, PopEditLab.class);
+                startActivity(intent);
             }
         });
+
+        editLab2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(TemplateLab.this, PopEditLab.class);
+                startActivity(intent);
+            }
+        });
+
+        deleteLab1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(TemplateLab.this, PopDelete.class);
+                startActivity(intent);
+            }
+        });
+
+        deleteLab2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(TemplateLab.this, PopDelete.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
