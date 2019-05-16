84744a46ff8eef900fbb4fe7453c31d473a8181c
conchoz <conchoz76@gmail.com>
2019-05-16 11:32:58 +0700

    edit prev eps igna

--- app/src/main/java/com/example/user/myapplication/PreviousEpisode.java
+++ app/src/main/java/com/example/user/myapplication/PreviousEpisode.java
@@ -10,11 +10,21 @@ import android.widget.LinearLayout;
 public class PreviousEpisode extends AppCompatActivity {
 
     Button addEpisodeBtn;
+    LinearLayout episode1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_previous_episode);
+
+        episode1 = (LinearLayout) findViewById(R.id.episode1);
+        episode1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PreviousEpisode.this, DetailPreviousEpisode.class);
+                startActivity(intent);
+            }
+        });
 
         addEpisodeBtn = (Button) findViewById(R.id.addEpisodeBtn);
         addEpisodeBtn.setOnClickListener(new View.OnClickListener() {
