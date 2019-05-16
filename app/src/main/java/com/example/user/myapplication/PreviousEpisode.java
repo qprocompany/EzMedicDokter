422bd4f28a0ff34ac17bf8baa1529fa4fa3054c5
conchoz <conchoz76@gmail.com>
2019-05-16 10:33:25 +0700

    edit prev eps igna

--- app/src/main/java/com/example/user/myapplication/PreviousEpisode.java
+++ app/src/main/java/com/example/user/myapplication/PreviousEpisode.java
@@ -1,13 +1,30 @@
 package com.example.user.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class PreviousEpisode extends AppCompatActivity {
+
+    Button addEpisodeBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_previous_episode);
+
+        addEpisodeBtn = (Button) findViewById(R.id.addEpisodeBtn);
+        addEpisodeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PreviousEpisode.this, DetailPasien.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
