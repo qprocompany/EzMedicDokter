802eaf2ca467073381c590a491c844bdd9537078
lievincent <vincentxxxlie@yahoo.com>
2019-05-16 10:58:16 +0700

    update by vincent

--- app/src/main/java/com/example/user/myapplication/DetailPasien2.java
+++ app/src/main/java/com/example/user/myapplication/DetailPasien2.java
@@ -4,30 +4,22 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.Spinner;
 
-public class DetailPasien extends AppCompatActivity {
-
+public class DetailPasien2 extends AppCompatActivity {
     Button next;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail_pasien);
+        setContentView(R.layout.activity_detail_pasien2);
 
         next = (Button) findViewById(R.id.next);
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(DetailPasien.this, DetailPasien2.class);
+                Intent intent = new Intent(DetailPasien2.this, DetailPasien3.class);
                 startActivity(intent);
             }
         });
-
-
     }
-
 }
--- app/src/main/java/com/example/user/myapplication/DetailPasien4.java
+++ app/src/main/java/com/example/user/myapplication/DetailPasien4.java
@@ -4,30 +4,22 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.Spinner;
 
-public class DetailPasien extends AppCompatActivity {
-
+public class DetailPasien4 extends AppCompatActivity {
     Button next;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail_pasien);
+        setContentView(R.layout.activity_detail_pasien4);
 
         next = (Button) findViewById(R.id.next);
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(DetailPasien.this, DetailPasien2.class);
+                Intent intent = new Intent(DetailPasien4.this, PopConfirm.class);
                 startActivity(intent);
             }
         });
-
-
     }
-
 }
