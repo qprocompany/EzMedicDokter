eed0a02e82e139da20137d734bae970f09e59727
lievincent <vincentxxxlie@yahoo.com>
2019-05-16 14:26:58 +0700

    update by vincent

--- app/src/main/java/com/example/user/myapplication/DetailPasien4.java
+++ app/src/main/java/com/example/user/myapplication/DetailPasien4.java
@@ -4,10 +4,14 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
-public class DetailPasien4 extends AppCompatActivity {
+public class DetailPasien4 extends AppCompatActivity{
     Button next;
+    Spinner spinnerDiagnosa, spinnerPilihan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,5 +25,8 @@ public class DetailPasien4 extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
     }
+
 }
